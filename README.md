8445357eaef7e0fb0bc8a08e1a9b4674019d9f87
Jayla Bullard <jaylabullard@Jaylas-MacBook-Air.local>
2021-04-16 22:06:52 -0400

    CSS/JS updates

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # MovieApp
+
+Uses an API to search popular movies and gives an overview/rating of each movie. 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d5aa2a05a1c924113bef57fbe3ae896f";
+const API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=d5aa2a05a1c924113bef57fbe3ae896f&page=1";
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
 
@@ -28,9 +28,30 @@ function showMovies (movies)
 
         const movieElement = document.createElement('div');
         movieElement.classList.add('movie');
-        movieElement.innerHTML = ``
+        movieElement.innerHTML = ` 
+            <img src="${IMG_PATH + poster_path}" alt="${title}>
+            <div class = "movie-info">
+                <h3>Movie Title</h3>
+                <span class = "${getClassByRate(vote_average)}"> ${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>`
 
+        main.appendChild(movieElement)
     })
+}
+
+function getClassByRate(vote){
+    
+    if(vote >= 8 ){
+        return "green"
+    } else if (vote >= 5){
+        return "orange"
+    }else {
+        return "red"
+    }
 }
 
 form.addEventListener('submit', (e) => {
